7c05697fe1d594d1077b32e4d122a8e05d420156
Chris Barber <chris@cb1inc.com>
2014-06-20 15:17:32 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+Ingot Web Server
+================
+
+Ingot Web Server is a service for the Ingot automation platform.
+
+## Installation
+
+It is intended to install the Ingot automation platform which will then
+install this service.
+
+	npm install -g ingot
+
+You will need to install the bower dependencies:
+
+	cd /path/to/ingot-web-server
+	node_modules/bower/bin/bower install
+
+## Reporting Bugs or submitting fixes
+
+If you run into problems, and trust us, there are likely plenty of them at this
+point -- please create an [Issue](https://github.com/appcelerator/ingot/issues)
+or, even better, send us a pull request.
+
+## Contributing
+
+Ingot is an open source project. Ingot wouldn't be where it is now without
+contributions by the community. Please consider forking Ingot to improve,
+enhance or fix issues. If you feel like the community will benefit from your
+fork, please open a pull request.
+
+To protect the interests of the Ingot contributors, Appcelerator, customers, and
+end users we require contributors to sign a Contributors License Agreement (CLA)
+before we pull the changes into the main repository. Our CLA is simple and
+straightforward - it requires that the contributions you make to any
+Appcelerator open source project are properly licensed and that you have the
+legal authority to make those changes. This helps us significantly reduce future
+legal risk for everyone involved. It is easy, helps everyone, takes only a few
+minutes, and only needs to be completed once.
+
+[You can digitally sign the CLA](http://bit.ly/app_cla) online. Please indicate
+your e-mail address in your first pull request so that we can make sure that
+will locate your CLA. Once you've submitted it, you no longer need to send one
+for subsequent submissions.
+
+## Legal
+
+Copyright (c) 2014 by [Appcelerator, Inc](http://www.appcelerator.com). All
+Rights Reserved. This project is licensed under the Apache Public License,
+version 2. Please see details in the LICENSE file.
---
+++ index.js
@@ -0,0 +1,131 @@
+var bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	express = require('express'),
+	favicon = require('serve-favicon'),
+	http = require('http'),
+	logger = require('morgan'),
+	responseTime = require('response-time'),
+
+	app = express(),
+	httpServer,
+	router = express.Router(),
+	running = false,
+	httpSockets = [];
+
+/**
+ * The Express app
+ */
+exports.app = app;
+
+/**
+ * A router that other services can tack on http handlers to
+ */
+exports.router = router;
+
+/**
+ * Homepage handler
+ */
+router.all('/', function (req, res) {
+	res.render('index');
+});
+
+/**
+ * Initializes the Express app
+ */
+exports.load = function load() {
+	app.locals.title = 'Appcelerator Web Server Service';
+
+	app.use(responseTime(5));
+
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'hjs');
+	app.set('layout', 'layouts/default');
+	app.engine('hjs', require('hogan-express'));
+
+	app.use(logger());
+	app.use(favicon(__dirname + '/public/img/favicon.png'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded());
+	app.use(cookieParser());
+	app.use(express.static(__dirname + '/public'));
+
+	app.use(router);
+
+	app.use(function (req, res, next) {
+		res.status(404);
+		res.render('404');
+	});
+
+	// development error handler
+	// will print stacktrace
+	if (app.get('env') === 'development') {
+		app.use(function (err, req, res, next) {
+			res.status(err.status || 500);
+			res.render('500', {
+				message: err.message,
+				error: err
+			});
+		});
+	}
+
+	// production error handler
+	// no stacktraces leaked to user
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('500', {
+			message: err.message,
+			error: {}
+		});
+	});
+};
+
+/**
+ * Starts the http server
+ * @param {Object} [cfg] - Configuration object
+ * @param {Function} [callback] - A function to call when the http server is running
+ */
+exports.start = function start(cfg, callback) {
+	if (running) return callback(new Error('Already running'));
+
+	if (typeof cfg === 'function') {
+		callback = cfg;
+		cfg = {};
+	} else if (!cfg) {
+		cfg = {};
+	}
+
+	var port = cfg['ingot-web-server'] && cfg['ingot-web-server'].port || 8080;
+	httpServer = http.createServer(app).listen(port, function () {
+		console.info('www: started server on port %d', port);
+		running = true;
+		typeof callback === 'function' && callback();
+	});
+
+	httpServer.on('connection', function (socket) {
+		httpSockets.push(socket);
+		socket.on('close', function () {
+			httpSockets.splice(httpSockets.indexOf(socket), 1);
+		});
+	});
+};
+
+/**
+ * Stops the http server
+ * @param {Function} [callback] - A function to call when the http server has stopped
+ */
+exports.stop = function stop(cfg, callback) {
+	if (!running) return callback();
+
+	for (var s; s = httpSockets.shift();) {
+		s.destroy();
+	}
+
+	httpServer.close(function () {
+		running = false;
+		callback();
+	});
+};
+
+exports.unload = function unload() {
+	//
+};
